b8d9c9b078aa98b8d81c792ba6bf1327c186a713
holyshared <holy.shared.design@gmail.com>
2016-08-07 18:21:33 +0900

    add echo response message

--- server.go
+++ server.go
@@ -3,11 +3,38 @@ package main
 import (
   "net/http"
   "io"
+  "encoding/json"
   "golang.org/x/net/websocket"
 )
 
+type Message struct {
+  Message string `json:"message"`
+}
+
+func (m *Message) WriteTo(w io.Writer) (n int64, err error) {
+  bytes, err := json.Marshal(m)
+  if err != nil {
+    return 0, err
+  }
+  length, werr := w.Write(bytes)
+
+  return int64(length), werr
+}
+
+/*
+func (m *Message) MarshalJSON() ([]byte, error) {
+  return json.Marshal(m)
+}
+*/
+func ResponseMessage(message string) *Message {
+  return &Message {
+    Message: message,
+  }
+}
+
 func EchoServer(ws *websocket.Conn) {
-  io.Copy(ws, ws);
+  message := ResponseMessage("ok!!")
+  message.WriteTo(ws)
 }
 
 func main() {
